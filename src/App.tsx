41bfbc2b52da6aa14b3f4c3a6a65cea43d25da55
dshuais <1137896420@qq.com>
2024-08-16 16:19:01 +0800

    ✨ feat: 引入DataV

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import { FullScreenContainer } from '@jiaminghi/data-view-react';
+import FullScreenContainer from '@jiaminghi/data-view-react/lib/fullScreenContainer';
+import Loading from '@jiaminghi/data-view-react/lib/loading';
+// import BorderBox1 from '@jiaminghi/data-view-react/lib/borderBox1';
+
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FullScreenContainer>
+      <Loading>加载中...</Loading>
+    </FullScreenContainer>
   );
 }
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+declare module '@jiaminghi/data-view-react/lib/fullScreenContainer'
+declare module '@jiaminghi/data-view-react/lib/loading'
